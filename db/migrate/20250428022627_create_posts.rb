85ad54f60e826d3302de83758152d8d6940bca20
Naokichi55 <ibosanganba@gmail.com>
2025-04-28 14:51:15 +0900

    add:scaffold実施後migrate実施

--- db/migrate/20250428022627_create_posts.rb
+++ db/migrate/20250428022627_create_posts.rb
@@ -2,7 +2,8 @@ class CreatePosts < ActiveRecord::Migration[7.2]
   def change
     create_table :posts do |t|
       t.string :title
-      t.text :body
+      t.text :body 
+
 
       t.timestamps
     end
